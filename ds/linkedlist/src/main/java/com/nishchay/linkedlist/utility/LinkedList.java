2cbb4f1478f8d00694f95e4b388493576c6a9e25
nishchay <rathi17nishchay@gmail.com>
2022-11-26 09:28:37 +0530

    Linked list add at specific index method is added and tested for exception

--- ds/linkedlist/src/main/java/com/nishchay/linkedlist/utility/LinkedList.java
+++ ds/linkedlist/src/main/java/com/nishchay/linkedlist/utility/LinkedList.java
@@ -7,6 +7,16 @@ public class LinkedList <T>{
 	
 	private Node<T> head;
 	private int count=0;
+	
+	public int size() {
+		int length=0;
+		Node<T> temp = head;
+ 		while(temp!=null) {
+			length++;
+			temp=temp.next;
+		}
+		return length;
+	}
 	public void add(T data) {
 		Node<T> temp = new Node<T>(data);
 		temp.next = head;
@@ -15,19 +25,26 @@ public class LinkedList <T>{
 	}
 	public void add(T data , int index) throws LinkedListIndexOutOfBound {
 		
-		if(index<0 || index > count)
+		int length = size();
+		if(index<=0 || index-1 > length)
 			throw new LinkedListIndexOutOfBound(ExceptionMesssages.IndexOutOfBound.getMessage());
 		int currentIndex=0;
 		Node<T> previousNode = null;
 		Node<T> currentNode = head; 
 		Node<T> newNode = new Node<T>(data);
-		while(currentIndex<index) {
+		while(currentIndex<index-1) {
 			previousNode=currentNode; 
 			currentNode=currentNode.next;
 			currentIndex++;
 		}
-		previousNode.next=newNode;
-		newNode.next=currentNode;
+		if(previousNode!=null) {
+			previousNode.next=newNode;
+			newNode.next=currentNode;
+		}
+		else {
+			newNode.next=head;
+			head=newNode;
+		}
 		
 	}
 	public void print() {
--- ds/linkedlist/src/test/java/com/nishchay/linkedlist/LinkedListTest.java
+++ ds/linkedlist/src/test/java/com/nishchay/linkedlist/LinkedListTest.java
@@ -1,13 +1,16 @@
 package com.nishchay.linkedlist;
 
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+
+import static org.junit.Assert.assertThrows;
 import static org.mockito.Mockito.when;
 
 import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
@@ -18,18 +21,39 @@ import com.nishchay.linkedlist.utility.LinkedList;
 
 @RunWith(MockitoJUnitRunner.class)
 public class LinkedListTest {
+	private LinkedList<Integer> list;
 	
+	@Rule
+	public ExpectedException expectedException = ExpectedException.none();
 	
+	@Before
+	public void init() {
+		list=new LinkedList<Integer>();
+		
+	}
 	
 	@Test
-	public void shouldReturnZeroIfListIsEmpty() {
-		System.out.println(ExceptionMesssages.IndexOutOfBound.getMessage());
+	public void addShouldSucced() throws LinkedListIndexOutOfBound {
+		
+		
+
 	}
 	
 	@Test
-	public void addShouldThrowExceptionWhenIndexIsOutOfBound() {
+	public void addShouldThrowExceptionWhenIndexIsOutOfBound() throws LinkedListIndexOutOfBound {		
+expectedException.expect(LinkedListIndexOutOfBound.class);
 		
+		//Below line check insertion in empty list
+		list.add(1,1);
+		//check insertion at the end of list
+		list.add(3,2);
 		
+		//check insertion at
 		
+		//below Line through the expected exception
+		list.add(4,4);
 	}
+	
+	
+	
 }
